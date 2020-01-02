c3d23b5a9187a6189b3f6e2546229984e2305fbd
Ali El Saleh <elsaleh78@gmail.com>
2020-01-02 13:33:30 +1100

    Add function to get all object pools from the world

--- Plugins/BulletPatternGenerator/Source/BulletPatternGenerator/Private/ObjectPooler/ObjectPoolFunctionLibrary.cpp
+++ Plugins/BulletPatternGenerator/Source/BulletPatternGenerator/Private/ObjectPooler/ObjectPoolFunctionLibrary.cpp
@@ -6,19 +6,33 @@
 
 #include "Kismet/GameplayStatics.h"
 
-
 AObjectPoolBase* UObjectPoolFunctionLibrary::GetObjectPool(const UObject* WorldContextObject, const FName PoolName)
 {
 	TArray<AActor*> FoundObjectPools;
 	UGameplayStatics::GetAllActorsOfClass(WorldContextObject, AObjectPoolBase::StaticClass(), FoundObjectPools);
 
-	for (auto ObjectPool : FoundObjectPools)
+	for (auto FoundObjectPool : FoundObjectPools)
 	{
-		const auto NewObjectPool = Cast<AObjectPoolBase>(ObjectPool);
+		const auto ObjectPool = Cast<AObjectPoolBase>(FoundObjectPool);
 		
-		if (NewObjectPool->GetPoolName() == PoolName)
-			return NewObjectPool;
+		if (ObjectPool->GetPoolName() == PoolName)
+			return ObjectPool;
 	}
 
 	return nullptr;
+}
+
+TArray<AObjectPoolBase*> UObjectPoolFunctionLibrary::GetAllObjectPools(const UObject* WorldContextObject)
+{
+	TArray<AActor*> FoundActors;
+	TArray<AObjectPoolBase*> FoundObjectPools;
+	UGameplayStatics::GetAllActorsOfClass(WorldContextObject, AObjectPoolBase::StaticClass(), FoundActors);
+
+	for (auto FoundActor : FoundActors)
+	{
+		const auto ObjectPool = Cast<AObjectPoolBase>(FoundActor);
+		FoundObjectPools.Add(ObjectPool);
+	}
+
+	return FoundObjectPools;
 }
