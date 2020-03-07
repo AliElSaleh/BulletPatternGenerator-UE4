b332a12d33e7b99223b4650c4ddaa70aafd8eea9
Ali El Saleh <elsaleh78@gmail.com>
2020-03-07 16:14:32 +1100

    Remove slate modules

--- Plugins/BulletPatternGenerator/Source/BulletPatternGenerator/BulletPatternGenerator.Build.cs
+++ Plugins/BulletPatternGenerator/Source/BulletPatternGenerator/BulletPatternGenerator.Build.cs
@@ -29,15 +29,12 @@ public class BulletPatternGenerator : ModuleRules
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
-			
 		
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"CoreUObject",
 				"Engine",
-				"Slate",
-				"SlateCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
